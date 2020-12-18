ec69e76d513c4b0273d044d3e977d4d6abcba197
Svante Pihl <hej@svantepihl.com>
2020-12-18 16:27:04 +0100

    testing branch domains

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ class HomeIndex extends React.Component {
                 </Helmet>
 
                 <Banner />
-
+                
                 <div id="main">
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
